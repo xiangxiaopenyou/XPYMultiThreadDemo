9567c3d4f799ab68f8802e081c9e06cdd9a7d3ce
zhangdu_imac <>
2020-04-22 17:12:54 +0800

    增加部分NSOperation/NSOperationQueue示例

--- XPYMultiThreadDemo/ViewController.m
+++ XPYMultiThreadDemo/ViewController.m
@@ -17,7 +17,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 /**
@@ -29,21 +28,26 @@
     //串行队列
     //dispatch_queue_t queue = dispatch_queue_create("queue", DISPATCH_QUEUE_SERIAL);
     dispatch_sync(queue, ^{
+        sleep(2);
         NSLog(@"task1 %@", [NSThread currentThread]);
     });
     dispatch_sync(queue, ^{
+        sleep(1);
         NSLog(@"task2 %@", [NSThread currentThread]);
     });
     dispatch_sync(queue, ^{
+        sleep(3);
         NSLog(@"task3 %@", [NSThread currentThread]);
     });
     dispatch_barrier_sync(queue, ^{
         NSLog(@"task barrier %@", [NSThread currentThread]);
     });
     dispatch_sync(queue, ^{
+        sleep(2);
         NSLog(@"task4 %@", [NSThread currentThread]);
     });
     dispatch_sync(queue, ^{
+        sleep(1);
         NSLog(@"task5 %@", [NSThread currentThread]);
     });
 }
@@ -108,7 +112,7 @@
         //模拟网络请求
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             sleep(2);
-            NSLog(@"complete task1 %@", [NSThread currentThread]);
+            NSLog(@"task1 %@", [NSThread currentThread]);
             dispatch_group_leave(group);
         });
     });
@@ -117,7 +121,7 @@
         //模拟网络请求
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             sleep(2);
-            NSLog(@"complete task2 %@", [NSThread currentThread]);
+            NSLog(@"task2 %@", [NSThread currentThread]);
             dispatch_group_leave(group);
         });
     });
@@ -126,7 +130,7 @@
         //模拟网络请求
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             sleep(2);
-            NSLog(@"complete task3 %@", [NSThread currentThread]);
+            NSLog(@"task3 %@", [NSThread currentThread]);
             dispatch_group_leave(group);
         });
     });
@@ -150,7 +154,7 @@
         //模拟网络请求
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             sleep(2);
-            NSLog(@"complete task1");
+            NSLog(@"task1 %@", [NSThread currentThread]);
             dispatch_semaphore_signal(semaphore);
         });
         dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
@@ -161,7 +165,7 @@
         //模拟网络请求
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             sleep(2);
-            NSLog(@"complete task2");
+            NSLog(@"task2 %@", [NSThread currentThread]);
             dispatch_semaphore_signal(semaphore);
         });
         dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
@@ -172,7 +176,7 @@
         //模拟网络请求
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             sleep(2);
-            NSLog(@"complete task3");
+            NSLog(@"task3, %@", [NSThread currentThread]);
             dispatch_semaphore_signal(semaphore);
         });
         dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
@@ -183,34 +187,118 @@
         });
     });
 }
-- (void)operation {
-    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-    queue.maxConcurrentOperationCount = 4;
-    NSBlockOperation *lastOperation = [NSBlockOperation blockOperationWithBlock:^{
+
+
+/// NSOperation同步执行任务
+- (void)operationSync {
+    NSBlockOperation *operation1 = [NSBlockOperation blockOperationWithBlock:^{
+        sleep(3);
+        NSLog(@"task1 %@", [NSThread currentThread]);
+        NSLog(@"main thread:%@", [NSThread mainThread]);
+    }];
+    [operation1 start];
+    
+    NSBlockOperation *operation2 = [NSBlockOperation blockOperationWithBlock:^{
+        sleep(2);
+        NSLog(@"task2 %@", [NSThread currentThread]);
+        NSLog(@"main thread:%@", [NSThread mainThread]);
+    }];
+    [operation2 start];
+    
+    NSBlockOperation *operation3 = [NSBlockOperation blockOperationWithBlock:^{
         sleep(1);
-        NSLog(@"complete last operation");
+        NSLog(@"task3 %@", [NSThread currentThread]);
+        NSLog(@"main thread:%@", [NSThread mainThread]);
+    }];
+    [operation3 start];
+}
+
+/// NSOperation和NSOperationQueue实现同步任务效果
+- (void)operationQueueSync {
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    // 设置并发数为1实现同步效果
+    queue.maxConcurrentOperationCount = 1;
+    NSBlockOperation *operation1 = [NSBlockOperation blockOperationWithBlock:^{
+        sleep(2);
+        NSLog(@"task1 %@", [NSThread currentThread]);
+    }];
+    NSBlockOperation *operation2 = [NSBlockOperation blockOperationWithBlock:^{
+        sleep(1);
+        NSLog(@"task2 %@", [NSThread currentThread]);
+    }];
+    NSBlockOperation *operation3 = [NSBlockOperation blockOperationWithBlock:^{
+        sleep(3);
+        NSLog(@"task3 %@", [NSThread currentThread]);
+    }];
+
+    [queue addOperation:operation1];
+    [queue addOperation:operation2];
+    [queue addOperation:operation3];
+}
+
+/// 网络请求数据返回以后再将相应队列任务加入到队列中
+- (void)operationQueue {
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    NSBlockOperation *lastOperation = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"last task");
     }];
     NSBlockOperation *requestOperation1 = [NSBlockOperation blockOperationWithBlock:^{
-        sleep(4);
-        NSLog(@"complete request1");
+        NSLog(@"task1");
     }];
     NSBlockOperation *requestOperation2 = [NSBlockOperation blockOperationWithBlock:^{
-        sleep(2);
-        NSLog(@"complete request2");
+        NSLog(@"task2");
     }];
     NSBlockOperation *requestOperation3 = [NSBlockOperation blockOperationWithBlock:^{
-        sleep(3);
-        NSLog(@"complete request3");
+        NSLog(@"task3");
     }];
     [lastOperation addDependency:requestOperation1];
     [lastOperation addDependency:requestOperation2];
     [lastOperation addDependency:requestOperation3];
     [queue addOperation:lastOperation];
-    [queue addOperation:requestOperation1];
-    [queue addOperation:requestOperation2];
-    [queue addOperation:requestOperation3];
     
+    [NSURLSession.sharedSession dataTaskWithURL:[NSURL URLWithString:@"https://www.csdn.net"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        [queue addOperation:requestOperation1];
+    }];
+    [NSURLSession.sharedSession dataTaskWithURL:[NSURL URLWithString:@"https://www.baidu.com"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        [queue addOperation:requestOperation2];
+    }];
+    [NSURLSession.sharedSession dataTaskWithURL:[NSURL URLWithString:@"https://github.com"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        [queue addOperation:requestOperation3];
+    }];
 }
+
+//- (void)test {
+//    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+//    dispatch_group_t group = dispatch_group_create();
+//    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+//    dispatch_group_async(group, queue, ^{
+//        dispatch_async(queue, ^{
+//            sleep(1);
+//            NSLog(@"模拟网络请求1");
+//            dispatch_semaphore_signal(semaphore);
+//        });
+//        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+//    });
+//    dispatch_group_async(group, queue, ^{
+//        dispatch_async(queue, ^{
+//            sleep(5);
+//            NSLog(@"模拟网络请求2");
+//            dispatch_semaphore_signal(semaphore);
+//        });
+//        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+//    });
+//    dispatch_group_async(group, queue, ^{
+//        dispatch_async(queue, ^{
+//            sleep(3);
+//            NSLog(@"模拟网络请求3");
+//            dispatch_semaphore_signal(semaphore);
+//        });
+//        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+//    });
+//    dispatch_group_notify(group, queue, ^{
+//        NSLog(@"全部网络请求完成后的操作");
+//    });
+//}
 
 #pragma mark - Table view data source
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -252,7 +340,15 @@
         }
             break;
         case 5: {
-            [self operation];
+            [self operationSync];
+        }
+            break;
+        case 6: {
+            [self operationQueueSync];
+        }
+            break;
+        case 7: {
+            [self operationQueue];
         }
             break;
         default:
@@ -268,6 +364,8 @@
                    @"GCD队列组处理先完成多任务再完成后面任务",
                    @"GCD队列组用group_enter和group_leave处理多个异步网络请求",
                    @"GCD队列组用信号量处理多个异步网络请求",
+                   @"NSOperation同步执行任务",
+                   @"NSOperation和NSOperationQueue实现同步任务效果",
                    @"NSOperationQueue处理多个操作完成后处理最后任务"];
     }
     return _items;
